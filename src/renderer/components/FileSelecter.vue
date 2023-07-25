2d9262e17538fc0d6057a469e2af3426ebbdc8ba
Maiwa <maiwa0126@gmail.com>
2023-07-25 22:56:53 +0800

    upload front & add axios

--- src/renderer/components/FileSelecter.vue
+++ src/renderer/components/FileSelecter.vue
@@ -1,27 +1,44 @@
 <template>
-    <input type="file" @change="handleFiles">
-    <input type="button" value="send" @click="getFile()">
+    <input type="file" @change="onFileSelected">
+    <input type="button" value="send" @click="onUpload()">
+
+    <video width="120" height="240" controls>
+        <source :src="videoFullPath" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
     name: "FileSelecter",
-    methods: {
-        getFile() {
-            alert('function called')
-        },
-        handleFiles: function (element) {
-            let file = element.target.files[0];
-            let reader = new FileReader();
-            reader.onload = () => {
-                this.convert(reader.result);
-            };
-            reader.readAsText(file);
-            alert('function called')
-        },
-        convert: function (str) {
-            //TODO
+    data() {
+        return {
+            file: ''
         }
+    },
+    methods: {
+        onUpload() {
+            let formData = new FormData();
+            formData.append('file', this.file);
+
+            axios.post('',
+                formData,
+                {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                }
+            ).then(function () {
+                console.log('SUCCESS!!');
+            })
+                .catch(function () {
+                    console.log('FAILURE!!');
+                });
+        },
+        onFileSelected(event) {
+            this.file = event.target.files[0];
+        },
     }
 }
 
