5815897a6a4cc3f54048e5bd4bec2028cbcd7389
Maiwa <maiwa0126@gmail.com>
2023-07-28 04:36:16 +0800

    add api connect

--- src/renderer/components/FileSelecter.vue
+++ src/renderer/components/FileSelecter.vue
@@ -2,13 +2,14 @@
     <input type="file" @change="onFileSelected">
     <input type="button" value="send" @click="onUpload()">
 
-    <video width="120" height="240" controls>
+    <!-- <video width="120" height="240" controls>
         <source :src="videoFullPath" type="video/mp4">
         Your browser does not support the video tag.
-    </video>
+    </video> -->
 </template>
 
 <script>
+
 import axios from 'axios'
 export default {
     name: "FileSelecter",
@@ -22,15 +23,17 @@ export default {
             let formData = new FormData();
             formData.append('file', this.file);
 
-            axios.post('',
+            axios.post('http://localhost:8000/upload/',
                 formData,
                 {
                     headers: {
                         'Content-Type': 'multipart/form-data'
                     }
                 }
-            ).then(function () {
+            ).then(function (data) {
                 console.log('SUCCESS!!');
+                alert("upload uuid filename:" + JSON.stringify(data.data.filename));
+
             })
                 .catch(function () {
                     console.log('FAILURE!!');
@@ -38,6 +41,8 @@ export default {
         },
         onFileSelected(event) {
             this.file = event.target.files[0];
+            console.log('SUCCESS!!');
+            console.log(this.file)
         },
     }
 }
