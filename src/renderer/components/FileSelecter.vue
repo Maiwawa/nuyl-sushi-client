5fcc88eeee7a6efadf97c2a1f44fafa35c7b3ed4
Maiwa <maiwa0126@gmail.com>
2023-09-15 19:42:17 +0800

    20230915

--- src/renderer/components/FileSelecter.vue
+++ src/renderer/components/FileSelecter.vue
@@ -1,51 +1,47 @@
 <template>
-    <input type="file" @change="onFileSelected">
-    <input type="button" value="send" @click="onUpload()">
-
-    <!-- <video width="120" height="240" controls>
-        <source :src="videoFullPath" type="video/mp4">
-        Your browser does not support the video tag.
-    </video> -->
+  <input type="file" @change="onFileSelect" accept="video/.mp4" />
+  <input type="button" value="send" @click="onUpload()" />
+  <video controls width="640" height="360" id="videoPlayer">
+    <source src="" type="video/mp4" />
+  </video>
 </template>
 
 <script>
-
-import axios from 'axios'
+import axios from "axios";
 export default {
-    name: "FileSelecter",
-    data() {
-        return {
-            file: ''
-        }
+  name: "FileSelecter",
+  data() {
+    return {
+      file: "",
+    };
+  },
+  methods: {
+    onUpload() {
+      let formData = new FormData();
+      formData.append("file", this.file);
+
+      axios
+        .post("http://localhost:8000/upload/", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          responseType: "blob",
+        })
+        .then(function (res) {
+          const videoUrl = URL.createObjectURL(res.data);
+          const videoPlayer = document.getElementById("videoPlayer");
+          videoPlayer.src = videoUrl;
+          alert("SUCCESS!!");
+        })
+        .catch(function (err) {
+          alert("FAILURE!!");
+          console.log(err);
+        });
     },
-    methods: {
-        onUpload() {
-            let formData = new FormData();
-            formData.append('file', this.file);
-
-            axios.post('http://localhost:8000/upload/',
-                formData,
-                {
-                    headers: {
-                        'Content-Type': 'multipart/form-data'
-                    }
-                }
-            ).then(function (data) {
-                console.log('SUCCESS!!');
-                alert("upload uuid filename:" + JSON.stringify(data.data.filename));
-
-            })
-                .catch(function () {
-                    console.log('FAILURE!!');
-                });
-        },
-        onFileSelected(event) {
-            this.file = event.target.files[0];
-            console.log('SUCCESS!!');
-            console.log(this.file)
-        },
-    }
-}
-
-
+    onFileSelect(e) {
+      this.file = e.target.files[0];
+      console.log(this.file);
+    },
+  },
+};
 </script>
